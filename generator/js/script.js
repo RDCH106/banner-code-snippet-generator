c12e757af487fe20076194a5e54601f1179eaf46
Rubén de Celis Hernández <rubendecelis@ingran.es>
2016-03-26 21:48:19 +0100

    Added banner code in string vars

--- generator/js/script.js
+++ generator/js/script.js
@@ -1,3 +1,6 @@
+var script = "<!-- Javascript Code --> <script> function copyToClipboard(object) {	object.focus();	object.select(); document.execCommand('copy'); alert('Copiado al Portapapeles');        } </script>";
+var html = "<!-- HTML Code --> <div style='width:250px; text-align:center'> <div name='banner-container' id='banner-container'>	<a title='Entrar en Mascando Bits' target='_blank' href='http://rdch106.hol.es/'><img width='100%' src='http://rdch106.hol.es/wp-content/uploads/2016/02/Mascando_Bits_banner.jpg'></a>	</div> <br> <div style='width:100%'> <textarea style='width:100%' name='banner' id='banner' rows='6' cols='20' onClick='copyToClipboard(this)' readonly='readonly'> <div name='banner-container' id='banner-container'> <a title='Entrar en Mascando Bits' target='_blank' href='http://rdch106.hol.es/'><img width='100%' src='http://rdch106.hol.es/wp-content/uploads/2016/02/Mascando_Bits_banner.jpg'></a></div></textarea> </div> <br> <input type='button' value='Copiar' style='width:100%' onClick=\"copyToClipboard(document.getElementById('banner'))\"> </div>"
+
 function generateBanner(){
     var generator_result = document.getElementById("generator-result");
     if (getParameterByName('banner_img')!= null && getParameterByName('height') != null && getParameterByName('width') != null){
