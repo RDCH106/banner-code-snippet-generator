4297e24bc5d5eeb6334ae79d1ca635e38ed4942f
Rubén de Celis Hernández <rubendecelis@ingran.es>
2016-03-28 04:43:52 +0200

    Added result box

--- generator/js/script.js
+++ generator/js/script.js
@@ -1,10 +1,25 @@
 var script = "<!-- Javascript Code --> <script> function copyToClipboard(object) {	object.focus();	object.select(); document.execCommand('copy'); alert('Copiado al Portapapeles');        } </script>";
 var html = "<!-- HTML Code --> <div style='width:250px; text-align:center'> <div name='banner-container' id='banner-container'>	<a title='Entrar en Mascando Bits' target='_blank' href='http://rdch106.hol.es/'><img width='100%' src='http://rdch106.hol.es/wp-content/uploads/2016/02/Mascando_Bits_banner.jpg'></a>	</div> <br> <div style='width:100%'> <textarea style='width:100%' name='banner' id='banner' rows='6' cols='20' onClick='copyToClipboard(this)' readonly='readonly'> <div name='banner-container' id='banner-container'> <a title='Entrar en Mascando Bits' target='_blank' href='http://rdch106.hol.es/'><img width='100%' src='http://rdch106.hol.es/wp-content/uploads/2016/02/Mascando_Bits_banner.jpg'></a></div></textarea> </div> <br> <input type='button' value='Copiar' style='width:100%' onClick=\"copyToClipboard(document.getElementById('banner'))\"> </div>"
 
+var script_code = "&lt;!-- Javascript Code --&gt; &lt;script&gt; function copyToClipboard(object) {	object.focus();	object.select(); document.execCommand('copy'); alert('Copiado al Portapapeles');        } &lt;/script&gt;";
+var html_code = "&lt;!-- HTML Code --&gt; &lt;div style='width:250px; text-align:center'&gt; &lt;div name='banner-container' id='banner-container'&gt;	&lt;a title='Entrar en Mascando Bits' target='_blank' href='http://rdch106.hol.es/'&gt;&lt;img width='100%' src='http://rdch106.hol.es/wp-content/uploads/2016/02/Mascando_Bits_banner.jpg'&gt;&lt;/a&gt;	&lt;/div&gt; &lt;br&gt; &lt;div style='width:100%'&gt; &lt;textarea style='width:100%' name='banner' id='banner' rows='6' cols='20' onClick='copyToClipboard(this)' readonly='readonly'&gt; &lt;div name='banner-container' id='banner-container'&gt; &lt;a title='Entrar en Mascando Bits' target='_blank' href='http://rdch106.hol.es/'&gt;&lt;img width='100%' src='http://rdch106.hol.es/wp-content/uploads/2016/02/Mascando_Bits_banner.jpg'&gt;&lt;/a&gt;&lt;/div&gt;&lt;/textarea&gt; &lt;/div&gt; &lt;br&gt; &lt;input type='button' value='Copiar' style='width:100%' onClick=\"copyToClipboard(document.getElementById('banner'))\"&gt; &lt;/div&gt;"
+
 function generateBanner(){
     var generator_result = document.getElementById("generator-result");
     if (getParameterByName('banner_img')!= null && getParameterByName('height') != null && getParameterByName('width') != null){
-        alert (getParameterByName('banner_img')+" "+getParameterByName('height')+" "+ getParameterByName('width'));
+        //Set input values
+        //html = html.replace("http://rdch106.hol.es/wp-content/uploads/2016/02/Mascando_Bits_banner.jpg", getParameterByName('banner_img'));
+        //html = html.replace("http://rdch106.hol.es/wp-content/uploads/2016/02/Mascando_Bits_banner.jpg", getParameterByName('banner_img'));
+        //html_code = html_code.replace("http://rdch106.hol.es/wp-content/uploads/2016/02/Mascando_Bits_banner.jpg", getParameterByName('banner_img'));
+        //html_code = html_code.replace("http://rdch106.hol.es/wp-content/uploads/2016/02/Mascando_Bits_banner.jpg", getParameterByName('banner_img'));
+        //visualize
+        generator_result.innerHTML = "<h4>Preview</h4>"
+        var snippet = script + html;
+        generator_result.innerHTML = generator_result.innerHTML + "<div id='preview'><center>"+ snippet + "</center></div><hr>";
+        generator_result.innerHTML = generator_result.innerHTML + "<h3>Copy this code in your Web:</h3>";
+        var snippet_code = script_code + html_code;
+        generator_result.innerHTML = generator_result.innerHTML + "<textarea style='width:100%' name='code' id='banner' rows='6' cols='20' onClick='this.select();' readonly='readonly'>" + snippet_code + "</textarea>"
+        //alert (getParameterByName('banner_img')+" "+getParameterByName('height')+" "+ getParameterByName('width'));
     }
 }
 
